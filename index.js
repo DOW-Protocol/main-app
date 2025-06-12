c0bb3864f0f84b98d0bccc13e9cc7d84dd8f8116
Rivaldi Murpia <rivaldimurpia1@gmail.com>
2025-06-12 04:12:47 +0000

    feat: script now fetches and displays transaction hashes

--- index.js
+++ index.js
@@ -6,18 +6,32 @@ const RPC_URL = "https://shape-mainnet.g.alchemy.com/v2/dk0hyao4gWu0HegBmtZEgydb
 
 // Fungsi utama untuk menjalankan program
 async function main() {
-  console.log("Mencoba menghubungkan ke Arbitrum...");
+  console.log("Menghubungkan ke Arbitrum untuk mengambil blok terbaru...");
 
-  // Membuat koneksi ke blockchain via RPC
   const provider = new ethers.JsonRpcProvider(RPC_URL);
 
   try {
-    // Mencoba mengambil nomor blok terakhir untuk tes koneksi
-    const blockNumber = await provider.getBlockNumber();
-    console.log(`✅ Koneksi berhasil! Blok terakhir adalah: ${blockNumber}`);
-  } catch (error) {
-    console.error("❌ Gagal terhubung:", error.message);
+  const block = await provider.getBlock('latest');
+  
+  console.log(`✅ Berhasil mengambil data dari Blok Nomor: ${block.number}`);
+  console.log("----------------------------------------------------");
+
+  const transactionHashes = block.transactions;
+
+  // Cek dulu ada transaksinya atau tidak
+  if (transactionHashes.length === 0) {
+    console.log("--> Tidak ada transaksi di dalam blok ini.");
+  } else {
+    console.log(`--> Ditemukan ${transactionHashes.length} transaksi. Menampilkan hingga 10 hash pertama:`);
+    const first10Hashes = transactionHashes.slice(0, 10);
+    first10Hashes.forEach((txHash, index) => {
+      console.log(`${index + 1}. ${txHash}`);
+    });
   }
+
+    } catch (error) {
+  console.error("❌ Gagal mengambil data blok:", error.message);
+    }
 }
 
 // Menjalankan fungsi utama
