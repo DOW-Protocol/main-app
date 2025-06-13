c70130cb6b6400716d81f45c8ee5f63972a0fb59
RivaldiMurpia <rivaldimurpia1@gmail.com>
2025-06-13 04:23:12 +0000

    feat: implement multi-address monitoring for major protocols

--- services/blockchain.js
+++ services/blockchain.js
@@ -1,8 +1,9 @@
 import { ethers } from "ethers";
 import { config } from '../config.js';
+import genericABI from "../genericABI.json" with { type: "json" }; // Tambahkan ini
 
 const provider = new ethers.JsonRpcProvider(config.RPC_URL);
-
+export const genericInterface = new ethers.Interface(genericABI); // Tambahkan ini
 export async function getLatestBlock() {
   try {
     const block = await provider.getBlock('latest', true);
