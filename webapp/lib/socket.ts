73aa1eaa9c3eeabb90b18f1543c8e806499837af
RivaldiMurpia <rivaldimurpia1@gmail.com>
2025-06-18 14:02:29 +0700

    Update socket.ts

--- webapp/lib/socket.ts
+++ webapp/lib/socket.ts
@@ -3,4 +3,6 @@ import { io } from "socket.io-client";
 
 const URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 // Kita buat satu koneksi di sini dan ekspor, jadi seluruh aplikasi akan pakai yang ini
-export const socket = URL ? io(URL) : io();
+export const socket = io(URL, {
+  autoConnect: false
+});
