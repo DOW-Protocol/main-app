e2695f8d0b316c06a0478e5c536e513349e5f1e6
RivaldiMurpia <rivaldimurpia1@gmail.com>
2025-06-18 13:47:55 +0700

    Update socket.ts

--- webapp/lib/socket.ts
+++ webapp/lib/socket.ts
@@ -1,6 +1,6 @@
 // file: webapp/lib/socket.ts
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3001";
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 // Kita buat satu koneksi di sini dan ekspor, jadi seluruh aplikasi akan pakai yang ini
-export const socket = io(URL);
+export const socket = URL ? io(URL) : io();
